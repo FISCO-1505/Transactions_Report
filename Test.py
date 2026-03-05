54ae74ff43670ac36e60aed72bea015fa8282deb
FISCO-1505 <servicios.corporativ@finaccess.mx>
2026-03-05 12:23:59 -0600

    Add dataframe

--- Test.py
+++ Test.py
@@ -8,3 +8,9 @@ st.title("Prueba Cloud")
 df = pd.DataFrame(np.random.randint(10,100,size=(10,3)), columns=["Col1", "Col2", "Col3"])
 
 st.dataframe(df)
+
+st.subheader("Otra prueba")
+
+df1 = pd.DataFrame(np.random.randint(10,100,size=(10,5)), columns=["Col1", "Col2", "Col3", "Col4", "Col5"])
+
+st.dataframe(df1)
