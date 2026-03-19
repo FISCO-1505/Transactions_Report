c35e7cccbda7786b6ec5e91abdf455d94e54c48f
Elificc <elizabeth.cervantes@finaccess.mx>
2026-03-19 15:43:26 -0600

    Cambios en la legibilidad del código

--- Transaction_report.py
+++ Transaction_report.py
@@ -36,7 +36,7 @@ def ensure_private_lib():
             token = st.secrets["GITHUB_TOKEN"]
             repo_url = f"git+https://{token}@github.com/FISCO-1505/Finaccess_Resources.git"
             
-            with st.spinner("Configurando recursos..."):
+            with st.spinner("Configuring resources..."):
                 try:
                     subprocess.check_call([
                         sys.executable, "-m", "pip", 
@@ -44,13 +44,13 @@ def ensure_private_lib():
                         repo_url
                     ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-                    st.success("Recursos configurados correctamente.")
+                    st.success("Resources configured correctly.")
                     import FISCO_Sources
                 except Exception as e:
-                    st.error(f"Error crítico: No se pudo instalar la librería.")
+                    st.error(f"Critical error: The library could not be installed.")
                     st.stop()
         else:
-            st.error("No se encontró GITHUB_TOKEN en los Secrets de Streamlit.")
+            st.error("GITHUB_TOKEN was not found in Streamlit's Secrets.")
             st.stop()
 
 # Ejecutar la función
@@ -74,11 +74,11 @@ def main():
     ruta_base = Path(__file__).resolve().parent
 
     # Llamada a tu librería para validar acceso
-    acceso_concedido = auth.verificar_acceso(st.secrets["PSW_STREAMLIT"], crypto)
+    acceso_concedido = auth.verificar_acceso(st.secrets["PSW_STREAMLIT"], crypto,"EN")
 
     if not acceso_concedido:
         # Aquí puedes poner un mensaje opcional o dejarlo en blanco
-        st.info("Por favor, inicia sesión en el menú lateral para continuar.")
+        st.info("Please log in via the side menu to continue.")
 
 
     else:
@@ -97,7 +97,7 @@ def main():
         # Ejecutar opción seleccionada
         if selection == "Generate report":
 
-   
+            # Columnas necesarias
             cols={"Trade Date" : "Trade Date", 
                 "Family Name" : "Client", 
                 "Transaction Type" : "Transaction Type", 
@@ -106,117 +106,112 @@ def main():
                 "Local To Base FX Rate" : "Local To Base FX Rate", 
                 "Net Amount Base" : "Net Amount Base",
                 "Referencia Movimiento" : "Referencia Movimiento"} 
+            
+            # Función para realizar el filtro de los datos
+            def filtrar(df, columnas):
+            
+                # Seleccionar columnas necesarias y renombrarlas
+                df = df[list(cols.keys())].rename(columns=cols)
+                
+                # Filtrar tipo de transacción
+                df_filtrado = df.query("`Transaction Type` in ['Addition', 'Withdrawal of Cash']")
 
+                # Eliminar NAN de la columna Referencia Movimiento
+                df_filtrado = df_filtrado[df_filtrado["Referencia Movimiento"].notna()]
+                
+                # Limpiar espacios a los datos de la columna Referencia Movimientos
+                df_filtrado["Referencia Movimiento"] = (
+                    df_filtrado["Referencia Movimiento"]
+                    .astype(str)
+                    .str.strip()
+                    .str.upper()
+                )
+                # Eliminar referencias de movimientos vacías
+                df_filtrado = df_filtrado[df_filtrado["Referencia Movimiento"] != ""]
+
+                # Excluir ciertos textos
+                mask_excluir = df_filtrado["Referencia Movimiento"].str.contains(
+                    r"\b(debit|internal|interest|card)\b",
+                    case=False,
+                    regex=True
+                    ) | df_filtrado["Referencia Movimiento"].str.match(
+                    r"(?i)^(transfer)$"
+                )
+
+                # Guardamos nuestra tabla filtrada
+                df_filtrado = df_filtrado[~mask_excluir] 
+
+                return df_filtrado
+            
+            # Titulo 
             st.title("Transaction Report")
 
-            # Subir archivo CSV
+            # Subir archivo tipo CSV o XLSX
             uploaded_file = st.file_uploader("Upload file", type=["csv", "xlsx"])
 
+            # Creamos un DataFrame sin valores
             df = None
 
             if uploaded_file:
 
+                # Variable que guarda el tipo de archivo
                 file_type = uploaded_file.name.split(".")[-1].lower()
 
-
+                # Leer el archivo y sustituye lo que había en df
                 if file_type == "csv":
                     df = pd.read_csv(uploaded_file, sep=None, engine="python")
                 else:
                     df = pd.read_excel(uploaded_file)
+                    # Aplicar formato a la columna de fecha
+                    df["Trade Date"] = df["Trade Date"].dt.strftime("%#d/%#m/%Y")
 
-                #st.dataframe(df)
-                df.columns = df.columns.str.strip()
-
-                st.success("File uploaded successfully")
-                #st.dataframe(df.head()) 
+                # Mensaje exitoso
+                st.success("File uploaded successfully") 
                 
             if df is not None:
 
+                # Lógica que seguirá el botón filtrar
                 if st.button("Filter file"):
 
+                    # Quitar espacios en los nombres de las columnas
+                    df.columns = df.columns.str.strip().str.replace(r"\s+", " ", regex=True)
+                    # Columnas necesarias para poder filtrar
                     required_cols = set(cols.keys())
-                    file_cols = set(df.columns)
+                    # Columnas faltantes
+                    missing_cols = required_cols - set(df.columns)
 
-                    missing_cols = required_cols - file_cols
-
+                    # Enviar mensajes de error si faltan columnas para realizar el filtro
                     if missing_cols:
 
                         st.error("❌ The file doesn't contain all the necessary columns")
-
                         st.write("The following columns are missing:")
                         for col in missing_cols:
+                            # Mostrar cuales son las columnas que faltan
                             st.write(f"- {col}")
 
                     else:
+                        # Realizar el filtro
+                        filtrar(df, cols)
+                        df_filtrado = filtrar(df, cols)
 
-                        st.success("✅ File ready to download")
-
-                        # Limpiar espacios en nombres de columnas
-                        df.columns = df.columns.str.strip().str.replace("  ", " ")
-                        
-                        
-                        # Seleccionar columnas necesarias y renombrarlas
-                        df = df[list(cols.keys())].rename(columns=cols) 
-                            
-                    
-                        # Filtrar tipo de transacción
-                        df_filtrado = df.query("`Transaction Type` in ['Addition', 'Withdrawal of Cash']")
-                    
-                        
-                        
-                        # Eliminar NAN 
-                        df_filtrado = df_filtrado[df_filtrado["Referencia Movimiento"].notna()]
-                    
-                        # Limpiar espacios
-                        df_filtrado["Referencia Movimiento"] = (
-                            df_filtrado["Referencia Movimiento"]
-                            .astype(str)
-                            .str.strip()
-                            .str.upper()
-                        )
-                    
-                        # Eliminar vacíos después del strip
-                        df_filtrado = df_filtrado[df_filtrado["Referencia Movimiento"] != ""]
-                    
-                        # Excluir ciertos textos
-                        mask_excluir = df_filtrado["Referencia Movimiento"].str.contains(
-                            r"\b(debit|internal|interest|card)\b",
-                            case=False,
-                            regex=True
-                            ) | df_filtrado["Referencia Movimiento"].str.match(
-                            r"(?i)^(transfer)$"
-                        )
-                    
-                        df_filtrado = df_filtrado[~mask_excluir]
-                        
-                        df_filtrado["Trade Date"] = pd.to_datetime(df_filtrado["Trade Date"],format="%m/%d/%Y")
-                        
-                        
+                        # Obtener fecha mínima y máxima 
                         fecha_min = df_filtrado['Trade Date'].min()
                         fecha_max = df_filtrado['Trade Date'].max()  
-                        format_fecha_min = f"{fecha_min.month}/{fecha_min.day}/{fecha_min.year}"
-                        format_fecha_max = f"{fecha_max.month}/{fecha_min.day}/{fecha_min.year}"
                         
-                        # Formatear sin ceros a la izquierda
-                        df_filtrado["Trade Date"] = df_filtrado["Trade Date"].apply(lambda x: f"{x.month}/{x.day}/{x.year}")
-                        
+                        # Obtener longitud de la referencia
                         ancho = df_filtrado['Referencia Movimiento'].str.len().max()
 
                     
                         # Crear archivo Excel en memoria
                         output = BytesIO()
                         with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
-                            df_filtrado.to_excel(writer, index=False, sheet_name="Filtered_data",startrow=1,header=False)
-                            
-                        
+                            df_filtrado.to_excel(writer, index=False, 
+                                                 sheet_name="Filtered_data",startrow=1,header=False)
                             
                             workbook  = writer.book
                             worksheet = writer.sheets["Filtered_data"]
-                        
-                            # Ocultar líneas de cuadrícula
-                            worksheet.hide_gridlines(0) #2
 
-                            # Anchos personalizados por columna
+                            # Ancho de cada columna 
                             column_widths = {
                                 "Trade Date": 14,
                                 "Client": 12,
@@ -238,19 +233,17 @@ def main():
                                 "Local To Base FX Rate": workbook.add_format({"align": "right", "valign": "vcenter", "font_name": "Lato Light", "font_size": 11, "font_color": "#000000","num_format": "0.00"}),
                                 "Net Amount Base": workbook.add_format({"align": "right", "valign": "vcenter", "font_name": "Lato Light", "font_size": 11, "font_color": "#000000","num_format": "General"}),
                                 "Referencia Movimiento": workbook.add_format({"align": "left", "valign": "vcenter", "font_name": "Lato Light", "font_size": 11, "font_color": "#000000"})
-                            }
-                            
-                            
+                            } 
                             
                             # Aplicar formato y ancho por columna
                             for col_num, col_name in enumerate(df_filtrado.columns):
-                                fmt = column_formats.get(col_name)  # formato con alineación
-                                width = column_widths.get(col_name, 20)  # ancho por defecto 20 si no está en el diccionario
+                                # formato con alineación
+                                fmt = column_formats.get(col_name)  
+                                # ancho de 20 
+                                width = column_widths.get(col_name, 20)  
                                 worksheet.set_column(col_num, col_num, width, fmt)
-
-                        
-                            #Formato
-                            # ===== Formato encabezado =====
+                            
+                            # Formato de encabezados
                             header_format = workbook.add_format({
                                 "bold": True,
                                 "font_name": "Lato Light",
@@ -263,17 +256,22 @@ def main():
                                 "text_wrap": True
                             })
                         
-                        
-                        
-                            # ===== Escribir encabezados con formato =====
+                            # Escribir encabezados  
                             for col_num, column in enumerate(df_filtrado.columns):
                                 worksheet.write(0, col_num, column, header_format)
                                 
                             
-                                
+                        # Muve el cursor al inicio               
                         output.seek(0)
-                
-                        nombre_archivo = st.text_input("File name", f"Transaction_{format_fecha_min}-{format_fecha_max}")
+
+                        # Mensaje listo para descargar
+                        st.success("✅ File ready to download")
+
+                        # Nombre predeterminado para guardar el archivo
+                        if fecha_min==fecha_max: 
+                            nombre_archivo = st.text_input("File name", f"Transaction_{fecha_min}")
+                        else: 
+                            nombre_archivo = st.text_input("File name", f"Transaction_{fecha_min}-{fecha_max}")
                         
                         # Botón para descargar
                         st.download_button(
@@ -283,7 +281,10 @@ def main():
                             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                         )
         else:
-             images.imagen_home("Advisors")  
+             # Cargar imagen
+             images.imagen_home("Advisors") 
+
+             # Instrucciones
              st.header("Instructions")
              st.info("Follow the steps carefully to complete the process")
              st.markdown("""
@@ -311,7 +312,7 @@ def main():
              
              
 
-
+        # Botón cerrar sesión
         if st.sidebar.button("Log out"):
             st.cache_data.clear()
             st.toast("Caché eliminada")
